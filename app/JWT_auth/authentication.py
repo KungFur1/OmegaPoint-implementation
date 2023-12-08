573b69812a761d8824ec26e242ca9a1eaad4e9f5
Joris Plaščinskas <PlascinskasJoris@gmail.com>
2023-12-08 16:21:57 +0200

    Disable password hashing

--- app/JWT_auth/authentication.py
+++ app/JWT_auth/authentication.py
@@ -6,10 +6,12 @@ _password_context = CryptContext(schemes=["bcrypt"], deprecated="auto") # Passwo
 
 # Returns an encrypted version of the password
 def get_password_hash(password : str) -> str:
-    return _password_context.hash(password)
+    #return _password_context.hash(password)
+    return password
 
 # Returns if the passwords match
 def verify_password(plain_password : str, hashed_password : str) -> bool:
-    return _password_context.verify(plain_password, hashed_password)
+    #return _password_context.verify(plain_password, hashed_password)
+    return plain_password == hashed_password
 
     
